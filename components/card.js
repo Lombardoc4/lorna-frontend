3a2f5ab91a538d50a600a9ec695d79b0b3492479
Cris Lom <bigc4997@gmail.com>
2021-11-20 22:42:32 -0500

    footer listen now links

---
+++ components/card.js
@@ -0,0 +1,31 @@
+import React from "react"
+import Link from "next/link"
+import NextImage from "./image"
+
+
+const RecordCard = ({ record }) => {
+  return (
+    <Link href={`/releases/${record.slug}.html`}>
+      <a className="d-flex flex-wrap text-decoration-none" id={record.slug}>
+        <div className="custom-card">
+          <NextImage image={record.cover} />
+          <h5 id="title" className="mt-3">
+            {record.name}
+          </h5>
+          <p id="release" className="mb-0">
+            {record.releaseDate}
+          </p>
+        </div>
+      </a>
+    </Link>
+  )
+}
+
+const Card = (props) => {
+
+  if (props.record) {
+    return <RecordCard record={props.record} />
+  }
+}
+
+export default Card
